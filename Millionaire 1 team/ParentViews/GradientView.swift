1ce5c8a3a6145fbd5a61384f6cce76b53d97265e
Konstantin Grachev <pibadubadidad@gmail.com>
2023-02-09 07:45:52 +0300

    fixed adding files

---
+++ Millionaire 1 team/ParentViews/GradientView.swift
@@ -0,0 +1,47 @@
+//
+//  GradientView.swift
+//  Millionaire 1 team
+//
+//  Created by Konstantin Grachev on 09.02.2023.
+//
+
+import UIKit
+
+class GradientView: UIView {
+    
+    private enum Constants {
+        static let cornerRadius: CGFloat = 10
+    }
+    
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+        dropShadow()
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+    
+    lazy var gradientLayer: CAGradientLayer = {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.borderColor = UIColor.gray.cgColor
+        gradientLayer.borderWidth = 1
+        let colors: [UIColor] = [#colorLiteral(red: 0.2530327737, green: 0.4159591794, blue: 0.5364745855, alpha: 1), #colorLiteral(red: 0.1300314665, green: 0.2300171852, blue: 0.3798839748, alpha: 1), #colorLiteral(red: 0.2530327737, green: 0.4159591794, blue: 0.5364745855, alpha: 1)]
+        gradientLayer.colors = colors.map { $0.cgColor }
+        gradientLayer.startPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.cornerRadius = Constants.cornerRadius
+        layer.insertSublayer(gradientLayer, at: 0)
+        return gradientLayer
+    }()
+    
+    private func setupUI() {
+        layer.cornerRadius = Constants.cornerRadius
+    }
+}
