81e7844710d2d1c2ff69f62052256df0327c0bb2
Konstantin Grachev <pibadubadidad@gmail.com>
2023-02-09 09:28:43 +0300

    fixed adding GameManager.swift

--- Millionaire 1 team/GameManager.swift
+++ Millionaire 1 team/GameManager.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+final class GameManager {
+    
+    let questions = Question.getQuestions()
+    
+    var levelsCounter = 1
+        
+    var levelQuestions: [Question] {
+        var questionsLevel = [Question]()
+        questions.forEach {
+            if $0.level == levelsCounter {
+                questionsLevel.append($0)
+            }
+        }
+        return questionsLevel
+    }
+    
+    func getCurrentQuestion() -> Question {
+        let currentQuestion = levelQuestions[Int.random(in: 0..<levelQuestions.count)]
+        return currentQuestion
+    }
+}
